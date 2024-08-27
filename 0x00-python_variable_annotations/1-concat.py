9c1bda9b3e6566928e7d195f60fc73e23b3a0999
Djimra NGARLEITA <dngarleita@gmail.com>
2024-08-27 03:27:30 +0300

    chore: Task 1 - Concatenate strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for string manipulation
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings
+
+    Args:
+        str1 (str): First string parameter, base
+        str2 (str): Second string parameter, to be appended to the 1st
+
+    Returns:
+        str: The str2 append to the str1
+    """
+    return str1 + str2
