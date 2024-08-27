5a7fe5ee65ef786f1f22f7ab89de3bc28cb2119c
Djimra NGARLEITA <dngarleita@gmail.com>
2024-08-27 03:14:11 +0300

    Initial commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Annotated module for addition
+"""
+
+def add(a: float, b: float) -> float:
+    """Function that add two floats passed as param
+
+    Args:
+        a (float): First paramater of the function
+        b (float): Second parameter of the function
+
+    Returns:
+        float: Sum of a and b
+    """
+    return a + b
