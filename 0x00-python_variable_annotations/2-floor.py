ef1b046d7f6b11cbc20f61fa0e641bf35e137445
Djimra NGARLEITA <dngarleita@gmail.com>
2024-08-27 03:36:44 +0300

    chore: Task 2 - Write a typed function that returns a floor of n

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+import math
+
+
+def floor(n: float) -> int:
+    """Floor the number passed as argument
+
+    Args:
+        n (float): The number to be floored
+
+    Returns:
+        int: The int returned after applying the floor function
+    """
+    return math.floor(n)
