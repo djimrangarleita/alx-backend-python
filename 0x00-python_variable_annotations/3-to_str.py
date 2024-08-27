3e2b98e31034d3b4b2be5e59623f1cf454c0117f
Djimra NGARLEITA <dngarleita@gmail.com>
2024-08-27 03:46:53 +0300

    chore: Task 3 - Cast a float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""String manipulation module
+"""
+
+def to_str(n: float) -> str:
+    """Turn a float passed as argument to a string
+
+    Args:
+        n (float): Float number to be turned into string
+
+    Returns:
+        str: Return string representation of n
+    """
+    return str(n)
