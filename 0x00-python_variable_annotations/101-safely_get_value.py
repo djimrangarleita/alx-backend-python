03e8d45fe742b91e253b5c14b6bb7d48e8b0f084
Djimra NGARLEITA <dngarleita@gmail.com>
2024-08-31 03:53:03 +0300

    fix: Document the function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,6 +8,16 @@ T = TypeVar('T')
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
+    """Use variable types
+
+    Args:
+        dct (Mapping): Dictionary
+        key (Any): Any type key va
+        default (Union[T, None], optional): Optional param. Defaults to None.
+
+    Returns:
+        Union[Any, T]: Return type any or T
+    """
     if key in dct:
         return dct[key]
     else:
