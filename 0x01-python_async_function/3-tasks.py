b3d8ca47c6b7538753068eca149de08138427051
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-11 02:36:57 +0300

    fix: Type annotation for compatibility with python 3.7

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,17 +2,16 @@
 """Wait task executions
 """
 import asyncio
-from typing import Awaitable
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable[float]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Get and return a coroutine task
 
     Args:
         max_delay (int): Maximum waiting time
 
     Returns:
-        Awaitable[float]: Coroutine task
+        asyncio.Task[float]: Coroutine task
     """
     return asyncio.Task(wait_random(max_delay))
