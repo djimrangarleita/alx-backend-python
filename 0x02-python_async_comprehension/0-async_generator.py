c298ae20e2e8570588fd22df09259e7949b84af7
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-11 03:30:18 +0300

    fix: Add type annotation to function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """Coroutine generator
 
     Yields:
